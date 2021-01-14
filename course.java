815926b6caa2a659267a8a1c622632a5116296ca
Zymion <718976528@qq.com>
2021-01-14 14:56:30 +0800

    commit 2

---
+++ course.java
@@ -0,0 +1,5 @@
+import java.util.*;
+class course{
+  String cno;    //课程编号
+  String cname;  //课程名称
+}
--- student.java
+++ student.java
@@ -2,4 +2,5 @@ import java.util.*
 class student{
   private string name;
   private string address;
+  private string email;   //修改的内容
 }
